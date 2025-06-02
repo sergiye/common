4448f9c263d92200a6d34a75bf23a957a0a3863b
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-06-02 16:28:52 +0300

    FileLogger

--- sergiye.Common/Logger.cs
+++ sergiye.Common/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace sergiye.Common {
 
@@ -12,8 +13,32 @@ namespace sergiye.Common {
 
     public event Action<string, StateKind, bool> OnNewLogEvent;
 
-    public void Log(string message, StateKind kind = StateKind.Log, bool newLine = true) {
+    public virtual void Log(string message, StateKind kind = StateKind.Log, bool newLine = true) {
       OnNewLogEvent?.Invoke(message, kind, newLine);
+    }
+  }
+
+  public class FileLogger: Logger, IDisposable {
+
+    private readonly StreamWriter writer;
+
+    public FileLogger(string logPath = null) {
+
+      if (string.IsNullOrEmpty(logPath))
+        logPath = Path.Combine(Path.GetDirectoryName(Updater.CurrentFileLocation), $"{Updater.ApplicationName}.log");
+      writer = new StreamWriter(logPath, true);
+    }
+
+    public void Dispose() {
+      writer?.Dispose();
+    }
+
+    public override void Log(string message, StateKind kind = StateKind.Log, bool newLine = true) {
+      if (newLine)
+        writer.Write($"\n{DateTime.Now:u} - ");
+      writer.Write(message);
+
+      base.Log(message, kind, newLine);
     }
   }
 }
